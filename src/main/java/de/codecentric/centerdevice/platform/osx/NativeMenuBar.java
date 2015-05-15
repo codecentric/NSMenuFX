e66306d6e44c93f5f36fd2bacb06d9269d4f051e
Jan Gassen <jan.gassen@centerdevice.com>
2015-05-15 18:31:11 +0200

    Removed useless abstract keyword

--- src/main/java/de/codecentric/centerdevice/platform/osx/NativeMenuBar.java
+++ src/main/java/de/codecentric/centerdevice/platform/osx/NativeMenuBar.java
@@ -6,30 +6,29 @@ import javafx.scene.control.MenuItem;
 
 public interface NativeMenuBar {
 
-	public abstract void setMenuBar(MenuBar bar);
+	public void setMenuBar(MenuBar bar);
 
-	public abstract void renameMenu(int menuIndex, String title);
+	public void renameMenu(int menuIndex, String title);
 
-	public abstract void renameMenuItem(int menuIndex, int menuItemIndex,
-			String title);
+	public void renameMenuItem(int menuIndex, int menuItemIndex, String title);
 
-	public abstract void insertMenuItems(int menuIndex, int menuItemIndex,
+	public void insertMenuItems(int menuIndex, int menuItemIndex,
 			MenuItem... jfxItems);
 
-	public abstract void insertMenuItem(int menuIndex, int menuItemIndex,
+	public void insertMenuItem(int menuIndex, int menuItemIndex,
 			MenuItem jfxItem);
 
-	public abstract void removeMenuItem(int menuIndex, int menuItemIndex);
+	public void removeMenuItem(int menuIndex, int menuItemIndex);
 
-	public abstract void replaceMenuItem(int menuIndex, int menuItemIndex,
+	public void replaceMenuItem(int menuIndex, int menuItemIndex,
 			MenuItem jfxItem);
 
-	public abstract void addMenuItems(int menuIndex, MenuItem... jfxItems);
+	public void addMenuItems(int menuIndex, MenuItem... jfxItems);
 
-	public abstract void addMenuItem(int menuIndex, MenuItem jfxItem);
+	public void addMenuItem(int menuIndex, MenuItem jfxItem);
 
-	public abstract void addMenu(Menu jfxMenu);
+	public void addMenu(Menu jfxMenu);
 
-	public abstract MenuBar getMenuBar();
+	public MenuBar getMenuBar();
 
 }
