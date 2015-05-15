0b4fff379b96d5074ca5015a8ee1556151843c01
Jan Gassen <jan.gassen@centerdevice.com>
2015-05-15 18:31:40 +0200

    Reformatted code

--- src/main/java/de/codecentric/centerdevice/platform/osx/convert/ToJavaFXConverter.java
+++ src/main/java/de/codecentric/centerdevice/platform/osx/convert/ToJavaFXConverter.java
@@ -19,73 +19,75 @@ import de.codecentric.centerdevice.platform.osx.action.NativeAction;
 
 public class ToJavaFXConverter {
 
-  private static final long sel_isEnabled = OS.sel_registerName("isEnabled");
+	private static final long sel_isEnabled = OS.sel_registerName("isEnabled");
 
-  public Menu convert(NSMenu menu) {
-    Menu jfxMenu = new Menu(menu.title().getString());
-    NSArray itemArray = menu.itemArray();
+	public Menu convert(NSMenu menu) {
+		Menu jfxMenu = new Menu(menu.title().getString());
+		NSArray itemArray = menu.itemArray();
 
-    for (long i = 0; i < itemArray.count(); i++) {
-      NSMenuItem item = new NSMenuItem(itemArray.objectAtIndex(i));
-      if (item.submenu() != null) {
-        jfxMenu.getItems().add(convert(item.submenu()));
-      } else {
-        jfxMenu.getItems().add(convert(item));
-      }
-    }
+		for (long i = 0; i < itemArray.count(); i++) {
+			NSMenuItem item = new NSMenuItem(itemArray.objectAtIndex(i));
+			if (item.submenu() != null) {
+				jfxMenu.getItems().add(convert(item.submenu()));
+			} else {
+				jfxMenu.getItems().add(convert(item));
+			}
+		}
 
-    return jfxMenu;
-  }
+		return jfxMenu;
+	}
 
-  public MenuItem convert(NSMenuItem item) {
-    if (item.isSeparatorItem()) {
-      return new SeparatorMenuItem();
-    }
+	public MenuItem convert(NSMenuItem item) {
+		if (item.isSeparatorItem()) {
+			return new SeparatorMenuItem();
+		}
 
-    MenuItem jfxItem = new MenuItem(item.title().getString());
-    jfxItem.setVisible(!item.isHidden());
-    jfxItem.setDisable(!isEnabled(item));
-    
-    if (item.action() > 0) {
-      jfxItem.setOnAction(new NativeAction(item.action(), item.target()));
-    }
+		MenuItem jfxItem = new MenuItem(item.title().getString());
+		jfxItem.setVisible(!item.isHidden());
+		jfxItem.setDisable(!isEnabled(item));
 
-    if (item.keyEquivalent() != null) {
-      jfxItem.setAccelerator(getKeyCombination(item));
-    }
+		if (item.action() > 0) {
+			jfxItem.setOnAction(new NativeAction(item.action(), item.target()));
+		}
 
-    return jfxItem;
-  }
+		if (item.keyEquivalent() != null) {
+			jfxItem.setAccelerator(getKeyCombination(item));
+		}
 
-  private boolean isEnabled(NSMenuItem item) {
-	return OS.objc_msgSend_bool(item.id, sel_isEnabled);
-  }
+		return jfxItem;
+	}
 
-  private KeyCodeCombination getKeyCombination(NSMenuItem item) {
-    KeyCode keyCode = KeyCode.getKeyCode(item.keyEquivalent().getString().toUpperCase());
-    if (keyCode == null) {
-      return null;
-    }
+	private boolean isEnabled(NSMenuItem item) {
+		return OS.objc_msgSend_bool(item.id, sel_isEnabled);
+	}
 
-    return new KeyCodeCombination(keyCode, getModifiers(item));
-  }
+	private KeyCodeCombination getKeyCombination(NSMenuItem item) {
+		KeyCode keyCode = KeyCode.getKeyCode(item.keyEquivalent().getString()
+				.toUpperCase());
+		if (keyCode == null) {
+			return null;
+		}
 
-  private Modifier[] getModifiers(NSMenuItem item) {
-    ArrayList<Modifier> modifiers = new ArrayList<Modifier>();
-    if ((item.keyEquivalentModifierMask() & OS.NSAlternateKeyMask) > 0) {
-      modifiers.add(KeyCombination.ALT_DOWN);
-    }
-    if ((item.keyEquivalentModifierMask() & OS.NSShiftKeyMask) > 0) {
-      modifiers.add(KeyCombination.SHIFT_DOWN);
-    }
-    if ((item.keyEquivalentModifierMask() & OS.NSCommandKeyMask) > 0) {
-      modifiers.add(KeyCombination.META_DOWN);
-    }
-    if ((item.keyEquivalentModifierMask() & OS.NSControlKeyMask) > 0) {
-      modifiers.add(KeyCombination.CONTROL_DOWN);
-    }
-    Modifier[] modifierArray = modifiers.toArray(new Modifier[modifiers.size()]);
-    return modifierArray;
-  }
+		return new KeyCodeCombination(keyCode, getModifiers(item));
+	}
+
+	private Modifier[] getModifiers(NSMenuItem item) {
+		ArrayList<Modifier> modifiers = new ArrayList<Modifier>();
+		if ((item.keyEquivalentModifierMask() & OS.NSAlternateKeyMask) > 0) {
+			modifiers.add(KeyCombination.ALT_DOWN);
+		}
+		if ((item.keyEquivalentModifierMask() & OS.NSShiftKeyMask) > 0) {
+			modifiers.add(KeyCombination.SHIFT_DOWN);
+		}
+		if ((item.keyEquivalentModifierMask() & OS.NSCommandKeyMask) > 0) {
+			modifiers.add(KeyCombination.META_DOWN);
+		}
+		if ((item.keyEquivalentModifierMask() & OS.NSControlKeyMask) > 0) {
+			modifiers.add(KeyCombination.CONTROL_DOWN);
+		}
+		Modifier[] modifierArray = modifiers.toArray(new Modifier[modifiers
+				.size()]);
+		return modifierArray;
+	}
 
 }
