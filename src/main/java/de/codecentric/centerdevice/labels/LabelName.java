ae8cd6ec1ede044bf3a3c0f5eb6dab89272cc87c
Jan Gassen <jan.gassen@centerdevice.com>
2016-02-20 19:21:38 +0100

    Add more labels to localization

--- src/main/java/de/codecentric/centerdevice/labels/LabelName.java
+++ src/main/java/de/codecentric/centerdevice/labels/LabelName.java
@@ -3,7 +3,8 @@ package de.codecentric.centerdevice.labels;
 public enum LabelName {
   HIDE("hide"), QUIT("quit"), ABOUT("about"), SHOW_ALL("show_all"), HIDE_OTHERS("hide_others"), MINIMIZE(
       "minimize"), ZOOM("zoom"), CLOSE_WINDOW("close_window"), BRING_ALL_TO_FRONT(
-      "bring_all_to_front"), CYCLE_THROUGH_WINDOWS("cycle_through_windows");
+      "bring_all_to_front"), CYCLE_THROUGH_WINDOWS("cycle_through_windows"), FILE("file"), WINDOW("window"), VIEW(
+      "view"), HELP("help"), EDIT("edit");
 
   private String propertyKey;
 
