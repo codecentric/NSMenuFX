e462e48af055bdb59b6429c730a758eb57ce15ee
Jan Gassen <jan.gassen@centerdevice.com>
2019-08-12 21:07:47 +0200

    Improve way of setting menu for test

--- src/test/java/de/codecentric/centerdevice/cocoa/SampleApp.java
+++ src/test/java/de/codecentric/centerdevice/cocoa/SampleApp.java
@@ -1,16 +1,27 @@
 package de.codecentric.centerdevice.cocoa;
 
+import com.sun.javafx.stage.StageHelper;
 import de.codecentric.centerdevice.javafx.NSMenuFX;
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.beans.value.WeakChangeListener;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+import java.util.concurrent.Executors;
 
 /**
  * Created by jan on 19/03/16.
@@ -30,20 +41,38 @@ public class SampleApp extends Application {
     button.setOnAction(action -> createNewStage());
     root.getChildren().add(button);
 
-    primaryStage.setScene(new Scene(root, 300, 200));
+    Scene scene = new Scene(root, 300, 200);
+    primaryStage.setScene(scene);
     primaryStage.requestFocus();
     primaryStage.setTitle(mainWindowTitle);
-    primaryStage.show();
 
+    primaryStage.addEventHandler(WindowEvent.WINDOW_SHOWN, (event) -> {
+      Executors.newSingleThreadExecutor().execute(() -> {
+        try {
+          Thread.sleep(1000);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+        System.out.println("Setting");
+        NSApplication.sharedApplication().setMainMenu(getNsMenu());
+      });
+    });
+
+    primaryStage.show();
+  }
+
+  private NSMenu getNsMenu() {
     Menu menu = new Menu("Menu");
-    MenuItem item1 = new MenuItem("Item1");
+    MenuItem item1 = new MenuItem("Quit");
+    item1.setAccelerator(new KeyCodeCombination(KeyCode.Q, KeyCombination.META_DOWN));
+    item1.setOnAction(e -> Runtime.getRuntime().exit(0));
     MenuItem item2 = new MenuItem("Item2");
     Menu item3 = new Menu("Item3");
     item3.getItems().add(new MenuItem("SubItem"));
     menu.getItems().addAll(item1, item2, item3);
 
-    NSMenuFX menuFX = new NSMenuFX(menu); // TODO: NSMenuFX not yet supporting submenus
-    
+    NSMenuFX menuFX = new NSMenuFX(menu);
+
     /*
      * This API has not been carved in stone. It'll probably always look a bit odd, as Cocoa has no MenuBar class but
      * uses a NSMenu instead. The most important thing however is to keep the properties connected so that once the
@@ -59,18 +88,7 @@ public class SampleApp extends Application {
     NSMenuItem mainMenu = NSMenuItem.alloc().init("Application");
     mainMenu.setSubmenu(menuFX.getNsMenu());
     menuBar.addItem(mainMenu);
-
-    // TODO: There seems to be a timing issue when setting a new menu bar before all code for showing the window
-    // has finished. In this case it might happen that the default menu bar will overwrite our menu again.
-    Platform.runLater(() -> {
-      try {
-        Thread.sleep(1000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-      NSApplication.sharedApplication().setMainMenu(menuBar);
-    });
-
+    return menuBar;
   }
 
   private void createNewStage() {
